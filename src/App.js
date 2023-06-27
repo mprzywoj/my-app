26fff758a1aa4c538700975f8aa01db13ce8c3a0
lancelot89 <37180623+lancelot89@users.noreply.github.com>
2023-06-27 12:27:59 +0200

    Podstawowa aplikacja

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Michal",
+      todoItems: [{ action: "Kupić kwiaty", done: false },
+                  { action: "Wziać buty", done: false },
+                  { action: "Zebrać bilety", done: true },
+                  { action: "Zadzwonić do Jurka", done: false }],
+      newItemText: ""
+    }
+  }
+
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  }
+
+  createNewTodo = () => {
+    if (!this.state.todoItems
+      .find(item => item.action === this.state.newItemText)){
+        this.setState({
+          // ... - operator rozproszenia, wyodrebnienie elem. tablicy
+          todoItems: [...this.state.todoItems, { action: this.state.newItemText, done: false }], newItemText: ""
+        });
+      }
+  }
+
+  toggleTodo = (todo) => this.setState({
+    todoItems: this.state.todoItems.map(item => item.action === todo.action ?
+      { ...item, done: !item.done } : item)
+  });
+
+  todoTableRows = () => this.state.todoItems.map(item =>
+    <tr key={item.action}>
+      <td>{item.action}</td>
+      <td>
+        <input type='checkbox' checked={item.done} onChange={() => this.toggleTodo(item)} />
+      </td>
+    </tr>);
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Michal" ? "Jakub" : "Michal"
+    })
+  }
+
+  render = () =>
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">
+        Lista zadan uzytkownika {this.state.userName}
+        (Liczba zadan: {this.state.todoItems.filter(t => !t.done).length})
+      </h4>
+      <div className='container-fluid'>
+        <div className='my-1'>
+          <input className='form-control' value={this.state.newItemText} onChange={this.updateNewTextValue} />
+          <button className='btn btn-primary mt-1' onClick={this.createNewTodo}>Dodaj</button>
+        </div>
+        <table className='table table-striped table-bordered'>
+          <thead>
+            <tr><th>Opis</th><th>Wykonane</th></tr>
+          </thead>
+          <tbody>{this.todoTableRows()}</tbody>
+        </table>
+      </div>
+        <button className='btn btn-primary m-2' onClick={this.changeStateData}>Zmien</button>
     </div>
-  );
+  
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div>
+//       <h4 className="bg-primary text-white text-center p-2">
+//         Lista zadan
+//       </h4>
+//     </div>
+//   );
+// }
